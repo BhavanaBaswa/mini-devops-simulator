1e4beb276524251f7d334305adffb8037796d3be
BhavanaBaswa <bhavanabaswa77@gmail.com>
2026-01-20 21:15:01 +0530

    Day 3: clean project structure and requirements file

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps Simulator 🚀"
+    env = os.getenv("DEPLOY_ENV", "development")
+    return f"Hello from DevOps Simulator 🚀 (Environment: {env})"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
